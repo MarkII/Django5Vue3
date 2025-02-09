de3dd356e6f81d3ecda0c7f345ddde505dccb1d7
MarkII <reyiczd@gmail.com>
2025-02-09 21:17:34 +0100

    model create of producto and categoria

--- mystore/producto/models.py
+++ mystore/producto/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    imagen = models.ImageField(null=True, blank=True, upload_to='img_productos/')
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name="productos")
+    
+    def __str__(self):
+        return self.nombre + str(self.precio)
